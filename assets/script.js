ded4da2d081f0836c896bd56208aef4a8f2d6809
lilcobby <jshairfield13@gmail.com>
2023-09-16 19:28:02 -0400

    function that creates buttons with text display from answer pool, question prompt created from quest pool, need true false buttons to lower time and display next

--- assets/script.js
+++ assets/script.js
@@ -3,12 +3,22 @@ var quest1 = document.getElementById("one");
 var quest2 = document.getElementById("two");
 var quest3 = document.getElementById("three");
 var quest4 = document.getElementById("four");
-var questprompt = document.getElementById("question");
-// need our questions
+var questBox = document.getElementById("question");
+
+// our questions answers 1-10 as variable arrays
+var answerPrompt1 = [
+  "this is a question",
+  "this is also a quesion",
+  "final question",
+  "jk this is final question",
+];
+// our question prompts 1-10
+var questionPrompt1 = ["this is me asking", "you a question", "about cats"];
 
 // timer functionality
 var timeEl = document.querySelector(".timer");
 var secondsLeft = 120;
+var t = "hello";
 function setTime() {
   // from 4-09-ins_timers-intervals
   var timeEl = document.querySelector(".timer");
@@ -27,19 +37,27 @@ function setTime() {
 // start button starts timer and adds list element text with buttons.
 var startButton = document.querySelector(".start-button");
 startButton.addEventListener("click", function () {
-  //   removes start button and p
+  //   removes start button and p, calls function to populate the button answers with text and the question with text from first set of quest and ans.
   opener1 = opener1.textContent = "";
   startButton.parentNode.remove();
   setTime();
-  quest1 = quest1.innerHTML += "<button> this just got added </button>";
-  quest2 = quest2.innerHTML += "<button> this has been added too </button>";
-  quest3 = quest3.innerHTML += "<button> this too </button>";
-  quest4 = quest4.innerHTML += "<button> this lastly </button>";
-  questprompt = questprompt.textContent += "Here is your first question";
+  appendButton("one", answerPrompt1[0]);
+  appendButton("two", answerPrompt1[1]);
+  appendButton("three", answerPrompt1[2]);
+  appendButton("four", answerPrompt1[3]);
+  questBox.textContent = questionPrompt1[0];
 });
 
 // end start button
 
-// need to create our question and our answer buttons these buttons reset the page to a new question answer pool and reduce our timer by 15 seconds on click event.target to id wrong and right answers. all buttons refresh, wrong reduces time
+// now we need each subsequent q/a with buttons that reduce time
 
-// end question and answer buttons
+// function to create answer buttons and question prompts.
+function appendButton(q, r) {
+  let buttonText = r;
+  let button = document.createElement("button");
+  button.textContent = buttonText;
+
+  button.setAttribute("type", "button");
+  document.getElementById(q).appendChild(button);
+}
