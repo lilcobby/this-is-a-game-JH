18b5220a6be3d445f2adc9817d8abcab3e6db763
lilcobby <jshairfield13@gmail.com>
2023-09-16 13:10:48 -0400

    added basic timer

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,18 @@
+// timer functionality
+var timeEl = document.querySelector(".timer");
+var secondsLeft = 120;
+function setTime() {
+  // from 3-09-ins_timers-intervals
+  var timerInterval = setInterval(function () {
+    secondsLeft--;
+    timeEl.textContent = secondsLeft;
+
+    if (secondsLeft === 0) {
+      clearInterval(timerInterval);
+      // add function to set the page to it's final form
+    }
+  }, 1000);
+}
+setTime();
+// end timer functionality
 
