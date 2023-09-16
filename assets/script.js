6ce7aa5d1c2536166e874d38a7cc580e3feb5464
lilcobby <jshairfield13@gmail.com>
2023-09-16 14:31:47 -0400

    start button functionality that creates a question and four button answers

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,40 @@
+// question buttons variables
+var quest1 = document.getElementById("one");
+var quest2 = document.getElementById("two");
+var quest3 = document.getElementById("three");
+var quest4 = document.getElementById("four");
+var questprompt = document.getElementById("question");
 // timer functionality
 var timeEl = document.querySelector(".timer");
 var secondsLeft = 120;
 function setTime() {
-  // from 3-09-ins_timers-intervals
+  // from 4-09-ins_timers-intervals
+  var timeEl = document.querySelector(".timer");
+  var secondsLeft = 120;
   var timerInterval = setInterval(function () {
     secondsLeft--;
     timeEl.textContent = secondsLeft;
 
     if (secondsLeft === 0) {
       clearInterval(timerInterval);
-      // add function to set the page to it's final form
     }
   }, 1000);
 }
-setTime();
 // end timer functionality
 
+// start button starts timer and adds list element text with buttons.
+var startButton = document.querySelector(".start-button");
+startButton.addEventListener("click", function () {
+  setTime();
+  quest1 = quest1.innerHTML += "<button> this just got added </button>";
+  quest2 = quest2.innerHTML += "<button> this has been added too </button>";
+  quest3 = quest3.innerHTML += "<button> this too </button>";
+  quest4 = quest4.innerHTML += "<button> this lastly </button>";
+  questprompt = questprompt.textContent += "Here is your first question";
+});
+
+// end start button
+
+// need to create our question and our answer buttons these buttons reset the page to a new question answer pool and reduce our timer by 15 seconds on click event.target to id wrong and right answers. all buttons refresh, wrong reduces time
+
+// end question and answer buttons
